f2dd56e6eae5971dfc7885ec3abc2d40885f5b57
Arjo Bruijnes <arjo@weirdwater.net>
2019-07-04 11:39:34 +0200

    Rapidfire the updatestream method in the hope the stream will hook

--- clients/presenter/components/streamVideo.tsx
+++ clients/presenter/components/streamVideo.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react'
+import { info } from '../../shared/logger';
 
 export interface StreamVideoProps {
   stream: MediaStream
 }
 
-export class StreamVideo extends React.Component<StreamVideoProps, {}> {
+export class StreamVideo extends React.Component<StreamVideoProps> {
   private video = React.createRef<HTMLVideoElement>()
 
   constructor(props: StreamVideoProps) {
     super(props)
+
     this.updateStream = this.updateStream.bind(this)
   }
 
@@ -21,12 +23,23 @@ export class StreamVideo extends React.Component<StreamVideoProps, {}> {
   }
 
   updateStream() {
-    if (this.video && this.video.current && this.video.current.srcObject !== this.props.stream) {
+    info('updateStream')
+
+    if (!this.video) {
+      info('updateStream: no video found...')
+      return window.setTimeout(() => {
+        info('calling updatestream after 500')
+        this.updateStream()
+      }, 500)
+    }
+
+    if (this.video.current && this.video.current.srcObject !== this.props.stream) {
+      info('updateStream: makes the cut')
       this.video.current.srcObject = this.props.stream
     }
   }
 
   render() {
-    return <video ref={this.video} playsInline autoPlay />
+    return <video ref={this.video} playsInline autoPlay onLoadedMetadata={this.updateStream} />
   }
 }
